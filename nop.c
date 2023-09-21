250806a231a6f3fc450f68104ddb9f88ff164be3
lanresdev <lanresdev18@gmail.com>
2023-09-21 16:40:41 -0700

    LIFO and FIFO

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  *f_nop- nothing
+  *@head: stacks head.
+  *@counter: line_number counter.
+  *Return: no returns.
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
